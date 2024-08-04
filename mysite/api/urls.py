86fa2db1a740a8fcc1c23d980719863ccbd7e301
Ting0887 <lutingyang@gmail.com>
2024-08-04 20:40:14 +0800

    add search function

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("newsarticles/", views.ArticleListCreate.as_view(), name="newsarticlepost-view-create"),
+    path('news_article_search/', views.ArticleListSearch.as_view(), name='article-search'),
     path('article/<int:pk>/', views.ArticleDetail.as_view(), name='article-detail'),
 ]
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from django.db.models import Q
+from rest_framework.response import Response
 from .models import NewsArticle
 from .serializers import NewsArticleSerializer
     
@@ -9,9 +11,20 @@ class ArticleListCreate(generics.ListCreateAPIView):
     def get_queryset(self):
         return NewsArticle.objects.all()
 
+class ArticleListSearch(generics.ListAPIView):
+    serializer_class = NewsArticleSerializer
+    
+    def get_queryset(self):
+        queryset = NewsArticle.objects.all()
+        query = self.request.query_params.get('q', None)
+        if query:
+            queryset = queryset.filter(Q(title__icontains=query) | Q(content__icontains=query))
+        return queryset
+
 class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = NewsArticle.objects.all()
     serializer_class = NewsArticleSerializer
 
 def ArticleList(request):
     return render(request, 'news_article_view.html')
+
